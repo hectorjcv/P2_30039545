9c5ba38030b41b0381e061dd18f3cc8345da471e
hectorjcv <hectorjesuscorreav@gmail.com>
2022-05-28 23:24:46 -0400

    tarea3

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const geoip = require('geoip-lite');
 const sqlite3=require('sqlite3').verbose();
 const http=require('http');
 const path = require('path');
-
+const geoip = require('geoip-lite');
 
 const db=path.join(__dirname,"db","bd.db");
 const db_run=new sqlite3.Database(db, err =>{ 
